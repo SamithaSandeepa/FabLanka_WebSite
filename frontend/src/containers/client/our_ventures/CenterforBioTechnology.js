d8491d8de1375e1d8f62ee7bc2205da71968b044
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2023-08-17 17:22:45 +0530

    Update CenterforBioTechnology.js

--- frontend/src/containers/client/our_ventures/CenterforBioTechnology.js
+++ frontend/src/containers/client/our_ventures/CenterforBioTechnology.js
@@ -39,30 +39,6 @@ const CenterforBioTechnology = () => {
             />
           </div>
         </div>
-
-        {/* Center for Industry 4.0 Section */}
-        <div className="mb-12 bg-white p-6 rounded-lg shadow-md">
-          <h2 className="text-2xl font-semibold mb-4">
-            Center for Industry 4.0
-          </h2>
-          <p>
-            Center for Industry 4.0 (C4I4) is a think tank specialized in
-            research work on Industry 4.0 Technologies and Digital
-            Transformation Management.
-          </p>
-          <p>
-            C4I4 will act as an independent research institute associated with
-            FabLanka Foundation.
-          </p>
-          <div className="mt-4">
-            {/* Replace with actual images */}
-            <img
-              src="https://www.europeansprings.com/wp-content/uploads/2022/11/iStock-1329665165-1024x576.jpg"
-              alt="Center for Industry 4.0"
-              className="w-50 rounded-lg shadow-md mx-auto"
-            />
-          </div>
-        </div>
       </main>
     </div>
   );
