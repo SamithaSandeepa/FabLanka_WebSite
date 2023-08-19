08208932ee4e347c945ddd76aa7a314aa33b4967
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2023-08-19 10:15:24 +0530

    Update Home.js

--- frontend/src/containers/client/Home.js
+++ frontend/src/containers/client/Home.js
@@ -91,7 +91,7 @@ const Home = () => {
         </div>
         <div className="w-2/5">
           <iframe
-            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FMakanduraFabLab&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FMakanduraFabLab%2F&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
             width="500"
             height="500"
             style={{ border: "none", overflow: "hidden" }}
