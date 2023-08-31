b5e67c25557cdb29eb1aa553284684342c7cac18
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2023-08-31 11:14:42 +0530

    Update Home.js

--- frontend/src/containers/client/Home.js
+++ frontend/src/containers/client/Home.js
@@ -91,7 +91,7 @@ const Home = () => {
         </div>
         <div className="w-2/5">
           <iframe
-            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FMakanduraFabLab%2F&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FFabLankaFoundation%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
             width="500"
             height="500"
             style={{ border: "none", overflow: "hidden" }}
@@ -108,7 +108,7 @@ const Home = () => {
       </div>
 
       <iframe
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FMakanduraFabLab%2F&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FFabLankaFoundation%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
         width="350"
         height="500"
         style={{ border: "none", overflow: "hidden" }}
